4030b5bd46568850c5e41a0744af6f9097d29719
Sergey Gromkov <sgromkov@gmail.com>
2017-12-26 11:14:21 +0300

    Lesson 4. Exercise 3. Created App.

---
+++ 04-routing-and-es6/homework/03/src/main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ShopApp from './components/ShopApp.jsx';
+
+ReactDOM.render(
+    <ShopApp />,
+    document.getElementById('content')
+);
